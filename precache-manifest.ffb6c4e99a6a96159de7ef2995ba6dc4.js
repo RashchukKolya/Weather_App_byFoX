5b7b6b514ebb1db2e7033389d96e908df066ee41
Nick <raschyk1993@gmail.com>
2019-01-23 12:37:15 +0200

    Updates

---
+++ precache-manifest.ffb6c4e99a6a96159de7ef2995ba6dc4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba3f73e2be4fad1376f239be26e2647e",
+    "url": "/static/media/Polonne.ba3f73e2.png"
+  },
+  {
+    "revision": "e50309c55ac9f4f8107b2e0b1b01263b",
+    "url": "/static/media/DefaultImg.e50309c5.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8eba92aa6e949c70279e",
+    "url": "/static/js/main.8eba92aa.chunk.js"
+  },
+  {
+    "revision": "68f5a36be2ebcbe751ae",
+    "url": "/static/js/1.68f5a36b.chunk.js"
+  },
+  {
+    "revision": "8eba92aa6e949c70279e",
+    "url": "/static/css/main.e5ca4cf4.chunk.css"
+  },
+  {
+    "revision": "68f5a36be2ebcbe751ae",
+    "url": "/static/css/1.529020e3.chunk.css"
+  },
+  {
+    "revision": "c555895886e40533053335114d80bc90",
+    "url": "/index.html"
+  }
+];
